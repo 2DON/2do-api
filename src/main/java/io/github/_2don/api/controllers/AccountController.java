1e8aa6bf7118aba98a6ceebf8170655c4df51bb0
wesauis <wesleya.isotton@gmail.com>
2020-09-27 16:23:03 -0300

    update account entity and sign-in route, create edit route

--- src/main/java/io/github/_2don/api/controllers/AccountController.java
+++ src/main/java/io/github/_2don/api/controllers/AccountController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import io.github._2don.api.models.Account;
 import io.github._2don.api.repositories.AccountJPA;
+import io.github._2don.api.utils.Patterns;
 
 @RestController
 @RequestMapping("/accounts")
@@ -30,14 +31,63 @@ public class AccountController {
 
   @PostMapping("/sign-up")
   @ResponseStatus(HttpStatus.CREATED)
-  public void signUp(@Validated @RequestBody Account account) {
-    account.setPassword(bcrypt.encode(account.getPassword()));
+  public void signUp(@RequestBody Account account) {
+    if (account.getEmail() == null || !Patterns.EMAIL.matches(account.getEmail())
+        || account.getPassword() == null || account.getPassword().length() <= 4) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+    }
 
     if (accountJPA.existsByEmail(account.getEmail())) {
       throw new ResponseStatusException(HttpStatus.CONFLICT);
     }
 
+    account.setPassword(bcrypt.encode(account.getPassword()));
+    account.setName(account.getEmail());
+    account.setAvatar(null);
+    account.setOptions(null);
+
     accountJPA.save(account);
+  }
+
+  @PatchMapping("/edit")
+  @ResponseStatus(HttpStatus.OK)
+  public Account edit(@AuthenticationPrincipal Long accountId, @RequestBody Account account) {
+    var stored = accountJPA.findById(accountId)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+    if (account.getEmail() != null) {
+      if (!Patterns.EMAIL.matches(account.getEmail())) {
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+      }
+      if (accountJPA.existsByEmail(account.getEmail())) {
+        throw new ResponseStatusException(HttpStatus.CONFLICT);
+      }
+
+      stored.setEmail(account.getEmail());
+    }
+
+    if (account.getPassword() != null) {
+      if (account.getPassword().length() <= 4) {
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+      }
+
+      stored.setPassword(bcrypt.encode(account.getPassword()));
+    }
+
+    if (account.getName() != null) {
+      stored.setName(account.getName());
+    }
+
+    if (account.getOptions() != null) {
+      stored.setOptions(account.getOptions());
+    }
+
+    // TODO: recieve image, check size, etc... BASE64? FILE?
+    // if (account.getAvatar() != null) {
+    // stored.setName(account.getName());
+    // }
+
+    return accountJPA.save(stored);
   }
 
   @GetMapping("/info")
--- src/main/java/io/github/_2don/api/models/Account.java
+++ src/main/java/io/github/_2don/api/models/Account.java
@@ -4,8 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import java.sql.Timestamp;
+import java.sql.Date;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
@@ -29,15 +29,29 @@ public class Account {
   @JsonProperty(access = Access.READ_ONLY)
   private Long id;
 
-  @NotNull
-  @Size(min = 4, max = 50)
   @Column(nullable = false, unique = true, length = 50)
   private String email;
 
-  @Size(min = 8, max = 60)
-  @Column(nullable = false, length = 60)
   @JsonProperty(access = Access.WRITE_ONLY)
   private String password;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(columnDefinition = "TEXT")
+  private String avatar;
+
+  @Column(nullable = false)
+  @JsonProperty(access = Access.READ_ONLY)
+  private Boolean premium = false;
+
+  private String options;
+
+  @JsonProperty(access = Access.READ_ONLY)
+  private Date deleteRequest;
+
+  @JsonProperty(access = Access.READ_ONLY)
+  private Timestamp createdAt = new Timestamp(System.currentTimeMillis());
 
   public Account(String email, String password) {
     this.email = email;
