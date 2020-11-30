c6607955f2c1d07c5266896c33a2565cbd8c7cc4
wesauis <wesleya.isotton@gmail.com>
2020-11-30 08:32:54 -0300

    task delete

--- src/main/java/io/github/_2don/api/step/StepJPA.java
+++ src/main/java/io/github/_2don/api/step/StepJPA.java
@@ -3,6 +3,7 @@ package io.github._2don.api.step;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,5 +16,8 @@ public interface StepJPA extends JpaRepository<Step, Long> {
   Optional<Step> findByIdAndTaskId(Long stepId, Long taskId);
 
   Optional<Step> findByIdAndTaskIdAndTaskProjectId(Long stepId, Long taskId, Long projectId);
+
+  @Transactional
+  long deleteByTaskId(Long taskId);
 
 }
